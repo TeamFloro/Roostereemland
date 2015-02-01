0f962947525fb2ca0743348676e1ffb52ebcad6e
Floris ten Hacken <Fleurus@hotmail.com>
2015-02-01 16:00:00 +0100

    Hele hoop gedaan, je weet wat

--- app/src/main/java/nl/floro/roostereemland/MainActivity.java
+++ app/src/main/java/nl/floro/roostereemland/MainActivity.java
@@ -54,14 +54,12 @@ public class MainActivity extends ActionBarActivity {
     static final ArrayList<String> Lokalen = new ArrayList<>();
     static final Calendar kalender = new GregorianCalendar();
     private ProgressDialog progressBar;
-    LinearLayout roosterLayout;
-
+//    LinearLayout roosterLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        roosterLayout = (LinearLayout) findViewById(R.id.roosterLayout);
         Klassen.addAll(Arrays.asList("-", "eg1a", "ehv1bCE", "ehv1cCE", "ehv1d", "ehv1e", "ehv1fSp", "emh1g", "emh1h", "emh1iSp", "eg2a", "eg2b", "ehv2cCE", "ehv2d", "ehv2e", "ehv2f", "ehv2g", "ehv2h", "em2a", "em2b", "em2cSp", "eg3aiP", "eg3b", "ev3c", "ev3d", "ev3eSp", "eh3a", "eh3b", "eh3c", "eh3d", "eh3eSp", "em3a", "em3b", "em3c", "evg4a", "evg4b", "evg4c", "eh4a", "eh4b", "eh4c", "eh4d", "em4a", "em4b", "em4c", "evg5a", "evg5b", "evg5c", "eh5a", "eh5b", "eh5c", "eh5d", "evg6a", "evg6b"));
         Docenten.addAll(Arrays.asList("Akn", "Ama", "Ate", "Bbw", "Bdw", "Bke", "Blb", "Bne", "Bpd", "Bre", "Brn", "Brs", "Brw", "Bss", "Bvg", "Bzd", "Ccg", "Ctn", "Dhg", "Dkb", "Dma", "Dmn", "Dnt", "Egl", "Enn", "Esf", "Faw", "Fns", "Fsf", "Ggw", "Gns", "Grb", "Grk", "Gsg", "Gtf", "Hah", "Hdb", "Heb", "Hgd", "Hge", "Hke", "Hld", "Hmd", "Hpa", "Hpe", "Hse", "Hte", "Hwg", "Hwi", "Jnn", "Joe", "Jot", "Jsl", "Khd", "Kld", "Klm", "Kom", "Krf", "Kru", "Kte", "Ktn", "Kvw", "Leg", "Lhw", "Lmw", "Lrb", "Mdg", "Mdn", "Mes", "Mhf", "Mlf", "Mnn", "Mrl", "Nvm", "Obe", "Olm", "Osn", "Osw", "Pif", "Pjs", "Pre", "Ptd", "Rcs", "Rif", "Rig", "Rlw", "Rml", "Rui", "Rvm", "Sbt", "Sch", "Sgn", "Shd", "Sis", "Skt", "Srn", "Sta", "Swn", "Ten", "Tmn", "Tmw", "Vds", "Vec", "Ves", "Vew", "Vhk", "Viw", "Vkb", "Vkn", "Vnl", "Vrt", "Vsk", "Wbe", "Wma", "Wmg", "Wms"));
         Lokalen.addAll(Arrays.asList("000", "005", "006", "007", "024", "034", "035", "036", "037", "103", "104", "116", "117", "118", "119", "122", "123", "124", "126", "127", "128", "129", "130", "202", "203", "204", "205", "208", "210", "215", "216", "217", "218", "219", "221", "222", "224", "225", "227", "228", "303", "304", "307", "309", "310", "311", "313", "401", "402", "403", "N1", "N10", "N2", "N6", "N7", "N8", "N9", "S1", "S2", "StZ", "StZ-", "T1", "T2", "T3", "T4"));
@@ -69,6 +67,10 @@ public class MainActivity extends ActionBarActivity {
 //        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 //        final RoosterFragment roosterFragment = new RoosterFragment();
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        TextView mededelingen = (TextView) findViewById(R.id.textView);
+        final WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings();
+        webView.setBackgroundColor(Color.TRANSPARENT);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Klassen);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -80,7 +82,19 @@ public class MainActivity extends ActionBarActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                getRooster(position);
+
+                int klasPositie = position;
+                String partURL = "/c/c0000";
+                if (klasPositie == 0) {
+                    return;
+                }
+                if (position >= 10) {
+                    partURL = "/c/c000";
+                }
+
+                webView.loadUrl("http://www.roostereemland.nl/dagrooster/" + getWeek() + partURL + klasPositie + ".htm");
+                webView.setBackgroundColor(Color.TRANSPARENT);
+
             }
 
             @Override
@@ -98,54 +112,50 @@ public class MainActivity extends ActionBarActivity {
         }
         return week;//
     }
-
-    public void getRoosterMededelingen() {
-        final TextView title = new TextView(getApplicationContext());
-        final TextView rooster = new TextView(getApplicationContext());
-
-        roosterLayout.removeAllViewsInLayout();
-        RoostereemlandApiClient.get("", false, null, new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                Element roosterDoc = Jsoup.parse(new String(responseBody)).body();
-
-                for (Node node : roosterDoc.childNodes()) {
-                    if (node.nodeName().equals("#comment")) {
-                        if (node.toString().trim().equals("<!-- EINDE OPMERKINGEN-->")) {
-                            System.out.println("hallo");
-                        }
-                        // Some output for testing ...
-                        System.out.println("=== Comment =======");
-                        System.out.println(node.toString().trim() + "faggot"); // 'toString().trim()' is only out beautify
-                        System.out.println("=== Childs ========");
-
-
-                        // Get the childs of the comment --> following nodes
-                        final List<Node> childNodes = node.siblingNodes();
-
-                        // Start- and endindex for the sublist - this is used to skip tags before the actual comment node
-                        final int startIdx = node.siblingIndex();   // Start index - start after (!) the comment node
-                        final int endIdx = childNodes.size();       // End index - the last following node
-                    }
-                    // if it's a comment we do something
-                }
-
-
-                title.setTextColor(Color.BLACK);
-                title.setGravity(Gravity.CENTER);
-                title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 38);
-                title.setText(roosterDoc.select("font[size=4]").text());
-
-                roosterLayout.addView(title);
-                roosterLayout.addView(rooster);
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                System.out.println(new String(responseBody));
-            }
-        });
-    }
+//
+//    public String getRoosterMededelingen() {
+//
+//        final String[] mededelingen = new String[1];
+//        RoostereemlandApiClient.get("", false, null, new AsyncHttpResponseHandler() {
+//            @Override
+//            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+//                Element roosterDoc = Jsoup.parse(new String(responseBody)).body();
+//
+//                for (Node node : roosterDoc.childNodes()) {
+//                    if (node.nodeName().equals("#comment")) {
+//                        if (node.toString().trim().equals("<!-- EINDE OPMERKINGEN-->")) {
+//                            System.out.println("hallo");
+//                        }
+//                        // Some output for testing ...
+//                        System.out.println("=== Comment =======");
+//                        System.out.println(node.toString().trim() + "faggot"); // 'toString().trim()' is only out beautify
+//                        System.out.println("=== Childs ========");
+//
+//
+//                        // Get the childs of the comment --> following nodes
+//                        final List<Node> childNodes = node.siblingNodes();
+//
+//                        // Start- and endindex for the sublist - this is used to skip tags before the actual comment node
+//                        final int startIdx = node.siblingIndex();   // Start index - start after (!) the comment node
+//                        final int endIdx = childNodes.size();       // End index - the last following node
+//                    }
+//                    // if it's a comment we do something
+//                }
+//
+//
+//
+//                ;
+//
+//            }
+//
+//            @Override
+//            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+//                System.out.println(new String(responseBody));
+//            }
+//        });
+//
+//        return "";
+//    }
     /*
     * 51/c/c00012.htm
     * 51/c/c00002.htm
@@ -154,55 +164,7 @@ public class MainActivity extends ActionBarActivity {
     * Fix dat hij voor een klas onder de tien er een 0 voor zet en daarna niks. F: DONE
     * Ik wil dit niet fixen met een slordige if else of een case... F: Jammer joh, DONE
     */
-    public void getRooster(int klasPositie) {
-        final WebView webview = new WebView(getApplicationContext());
-//        final TextView rooster = new TextView(getApplicationContext());
 
-        String partURL = "/c/c0000";
-        if (klasPositie == 0) {
-            getRoosterMededelingen();
-            return;
-        }
-        if (klasPositie >= 10) {
-            partURL = "/c/c000";
-        }
-
-        roosterLayout.removeAllViewsInLayout();
-
-        WebSettings settings = webview.getSettings();
-        settings.setJavaScriptEnabled(true);
-        webview.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-
-        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-
-        progressBar = ProgressDialog.show(MainActivity.this, "WebView Example", "Loading...");
-
-        webview.setWebViewClient(new WebViewClient() {
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-
-            public void onPageFinished(WebView view, String url) {
-                if (progressBar.isShowing()) {
-                    progressBar.dismiss();
-                }
-            }
-
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                Toast.makeText(getApplicationContext(), "Oh no! " + description, Toast.LENGTH_SHORT).show();
-                alertDialog.setTitle("Error");
-                alertDialog.setMessage(description);
-                alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        return;
-                    }
-                });
-                alertDialog.show();
-            }
-        });
-        webview.loadUrl("http://www.roostereemland.nl/");
-    }
 
     // Leeg het layout omdat hij er anders steeds een view achter zet.
 
