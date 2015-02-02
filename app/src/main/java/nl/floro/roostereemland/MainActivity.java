6076823dbd48e51db3a803cdf4b4b1eb53eacb28
Floris ten Hacken <Fleurus@hotmail.com>
2015-02-02 13:04:36 +0100

    Mededelingen gefixt, docenten spinner werk ik aan. Spinners in Relativelayout gezet.

--- app/src/main/java/nl/floro/roostereemland/MainActivity.java
+++ app/src/main/java/nl/floro/roostereemland/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.Menu;
@@ -21,6 +22,7 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import org.apache.http.Header;
 import org.jsoup.Jsoup;
+import org.jsoup.helper.StringUtil;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
@@ -43,29 +45,32 @@ public class MainActivity extends ActionBarActivity {
     static final ArrayList<String> Lokalen = new ArrayList<>();
     static final Calendar kalender = new GregorianCalendar();
     private ProgressDialog progressBar;
+    static String p;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getRoosterMededelingen();
         AdView adView = (AdView) this.findViewById(R.id.adView);
-        TextView mededelingen = (TextView) findViewById(R.id.textView);
-        getRoosterMededelingen(mededelingen);
         AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
         adView.loadAd(adRequest);
         Klassen.addAll(Arrays.asList("-", "eg1a", "ehv1bCE", "ehv1cCE", "ehv1d", "ehv1e", "ehv1fSp", "emh1g", "emh1h", "emh1iSp", "eg2a", "eg2b", "ehv2cCE", "ehv2d", "ehv2e", "ehv2f", "ehv2g", "ehv2h", "em2a", "em2b", "em2cSp", "eg3aiP", "eg3b", "ev3c", "ev3d", "ev3eSp", "eh3a", "eh3b", "eh3c", "eh3d", "eh3eSp", "em3a", "em3b", "em3c", "evg4a", "evg4b", "evg4c", "eh4a", "eh4b", "eh4c", "eh4d", "em4a", "em4b", "em4c", "evg5a", "evg5b", "evg5c", "eh5a", "eh5b", "eh5c", "eh5d", "evg6a", "evg6b"));
         Docenten.addAll(Arrays.asList("Akn", "Ama", "Ate", "Bbw", "Bdw", "Bke", "Blb", "Bne", "Bpd", "Bre", "Brn", "Brs", "Brw", "Bss", "Bvg", "Bzd", "Ccg", "Ctn", "Dhg", "Dkb", "Dma", "Dmn", "Dnt", "Egl", "Enn", "Esf", "Faw", "Fns", "Fsf", "Ggw", "Gns", "Grb", "Grk", "Gsg", "Gtf", "Hah", "Hdb", "Heb", "Hgd", "Hge", "Hke", "Hld", "Hmd", "Hpa", "Hpe", "Hse", "Hte", "Hwg", "Hwi", "Jnn", "Joe", "Jot", "Jsl", "Khd", "Kld", "Klm", "Kom", "Krf", "Kru", "Kte", "Ktn", "Kvw", "Leg", "Lhw", "Lmw", "Lrb", "Mdg", "Mdn", "Mes", "Mhf", "Mlf", "Mnn", "Mrl", "Nvm", "Obe", "Olm", "Osn", "Osw", "Pif", "Pjs", "Pre", "Ptd", "Rcs", "Rif", "Rig", "Rlw", "Rml", "Rui", "Rvm", "Sbt", "Sch", "Sgn", "Shd", "Sis", "Skt", "Srn", "Sta", "Swn", "Ten", "Tmn", "Tmw", "Vds", "Vec", "Ves", "Vew", "Vhk", "Viw", "Vkb", "Vkn", "Vnl", "Vrt", "Vsk", "Wbe", "Wma", "Wmg", "Wms"));
         Lokalen.addAll(Arrays.asList("000", "005", "006", "007", "024", "034", "035", "036", "037", "103", "104", "116", "117", "118", "119", "122", "123", "124", "126", "127", "128", "129", "130", "202", "203", "204", "205", "208", "210", "215", "216", "217", "218", "219", "221", "222", "224", "225", "227", "228", "303", "304", "307", "309", "310", "311", "313", "401", "402", "403", "N1", "N10", "N2", "N6", "N7", "N8", "N9", "S1", "S2", "StZ", "StZ-", "T1", "T2", "T3", "T4"));
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        Spinner spinnerKlas = (Spinner) findViewById(R.id.spinner);
+        Spinner spinnerDocent = (Spinner) findViewById(R.id.spinner1);
         final WebView webView = (WebView) findViewById(R.id.webView);
         webView.getSettings();
         webView.setBackgroundColor(Color.TRANSPARENT);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Docenten);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Klassen);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        spinnerKlas.setAdapter(adapter);
+        spinnerDocent.setAdapter(adapter1);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinnerKlas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
@@ -88,6 +93,32 @@ public class MainActivity extends ActionBarActivity {
 
             }
         });
+
+        spinnerDocent.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                int klasPositie = position;
+                String partURL = "/c/c0000";
+                if (klasPositie == 0) {
+                    return;
+                }
+                if (position >= 10) {
+                    partURL = "/c/c000";
+                }
+
+                webView.loadUrl("http://www.roostereemland.nl/dagrooster/" + getWeek() + partURL + klasPositie + ".htm");
+                webView.setBackgroundColor(Color.TRANSPARENT);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 
     public String getWeek() {
@@ -99,8 +130,10 @@ public class MainActivity extends ActionBarActivity {
         return week;
     }
 
-    public void getRoosterMededelingen(final TextView tv) {
+    public void getRoosterMededelingen() {
+        final TextView mededelingen = (TextView) findViewById(R.id.textView1);
         RoostereemlandApiClient.get("", false, null, new AsyncHttpResponseHandler() {
+
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 Element roosterDoc = Jsoup.parse(new String(responseBody)).body();
@@ -126,7 +159,23 @@ public class MainActivity extends ActionBarActivity {
                     // if it's a comment we do something
                 }
 
-                tv.setText(roosterDoc.select("font[size=4]").text());
+                p = roosterDoc.select("font[size=4]").text();
+                if (p.contains("Roosterwijzer")) {
+                    char[] d;
+                    d = p.toCharArray();
+                    System.out.println(d[0]);
+
+                    for (int x = 0; x < d.length; x++) {
+                        if (d[x] == 'R' && d[x + 1] == 'o' && d[x + 2] == 'o' && d[x + 3] == 's' && d[x + 4] == 't' && d[x + 5] == 'e' && d[x + 6] == 'r' && d[x + 7] == 'w' && d[x + 8] == 'i' && d[x + 9] == 'j' && d[x + 10] == 'z' && d[x + 11] == 'e' && d[x + 12] == 'r') {
+                            p = p.substring(0, x);
+                            mededelingen.setText(p);
+                            mededelingen.setTextColor(0xffff0000);
+                            break;
+
+                        }
+                    }
+                } else System.out.println("hoisdfdsf opent m noiit");
+
 
             }
 
@@ -135,6 +184,8 @@ public class MainActivity extends ActionBarActivity {
                 System.out.println(new String(responseBody));
             }
         });
+
+        return;
     }
     /*
     * 51/c/c00012.htm
