8f111e00936a03c6ff6bf93ee6bbb6454d1002cb
Robert-Jan Keizer <robertjankeizer@gmail.com>
2015-02-02 13:30:02 +0100

    Changed a lot!

--- api/index.php
+++ api/index.php
@@ -1,14 +1,9 @@
 <?php 
 include_once "simple_html_dom.php";
-$html = file_get_html('http://www.google.com/');
+$html = file_get_html('http://roostereemland.nl/');
 
-// Find all images
-foreach($html->find('img') as $element) {
-	echo $element->src . '<br>';
-}
 
-// Find all links
-foreach($html->find('a') as $element) {
-	echo $element->href . '<br>'; 
+foreach ($elements->find('HR') as $child) {
+    echo end($child);
 }
 ?>
